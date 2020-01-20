3d2d8b808a39032248e6bc2f6c09bc7ff50a24d6
Eduard Tamsa <eduard.t@skyx.com>
2020-01-20 17:40:31 +0200

    Update mysql single instance deployment to have initial data

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,13 @@ echo -n '1f2d1e2e67df' | base64 > ./password_root.txt
 
 kubectl create secret generic db-user-pass --from-file=./username.txt --from-file=./password.txt --from-file=./password_root.txt
 
+kubectl apply -f ./mysql/configMap.yaml
+
 kubectl apply -f ./mysql/preMysql.yaml
 
 kubectl apply -f ./mysql/mysqlSi.yaml
+
+
 
 kubectl apply -f ./simpleApp/backend.yaml
 
