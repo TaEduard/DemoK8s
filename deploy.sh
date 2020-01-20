c1bf1ce5f6cae6465b9daf615397ca334f7272b9
Eduard Tamsa <eduard.t@skyx.com>
2020-01-20 14:44:24 +0200

    Remove out of scope content from this repo

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,10 @@ echo -n '1f2d1e2e67df' | base64 > ./password_root.txt
 
 kubectl create secret generic db-user-pass --from-file=./username.txt --from-file=./password.txt --from-file=./password_root.txt
 
+kubectl apply -f ./mysql/preMysql.yaml
+
+kubectl apply -f ./mysql/mysqlSi.yaml
+
+kubectl apply -f ./simpleApp/backend.yaml
+
+kubectl apply -f ./simpleApp/frontend.yaml
