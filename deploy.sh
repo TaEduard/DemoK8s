5a4d0ea1af26aa0f892761cc707d84829eb3d92c
Eduard Tamsa <eduard.t@skyx.com>
2020-01-22 00:56:33 +0200

    Add jenkins deployment

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo -n 'admin' | base64 > ./username.txt
 echo -n '1f2d1e2e67df' | base64 > ./password.txt
 echo -n '1f2d1e2e67df' | base64 > ./password_root.txt
 
-kubectl create secret generic db-user-pass --from-file=./username.txt --from-file=./password.txt --from-file=./password_root.txt
+kubectl create secret generic mysecret --from-file=./username.txt --from-file=./password.txt --from-file=./password_root.txt
 
 kubectl apply -f ./mysql/configMap.yaml
 
@@ -13,6 +13,6 @@ kubectl apply -f ./mysql/mysqlSi.yaml
 
 
 
-kubectl apply -f ./simpleApp/backend.yaml
+# kubectl apply -f ./simpleApp/backend.yaml
 
-kubectl apply -f ./simpleApp/frontend.yaml
+# kubectl apply -f ./simpleApp/frontend.yaml
